17c153814eb74fabba0d1ef366c2da59641af24e
Jason Fry <funkytomato@gmail.com>
2019-07-10 12:03:58 +0100

    Script for attaching objects to agent's hand

---
+++ RiotGame/Assets/Game/Scripts/AttachObjectToHandScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachObjectToHandScript : MonoBehaviour
+{
+    public GameObject objectToAttach;
+
+    private GameObject rightHand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //var userControl = FindObjectOfType<AirplaneUserControl>();
+        //Rigidbody targetRb = userControl.GetComponent<Rigidbody>();
+        //Debug.Log("Aircraft velocity: " + targetRb.velocity);
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
