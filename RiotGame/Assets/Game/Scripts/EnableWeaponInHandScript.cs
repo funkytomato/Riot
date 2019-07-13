1e10b2d6228ecc8b03b57a6eae90a8a4ee8f9c85
Jason Fry <funkytomato@gmail.com>
2019-07-13 12:26:01 +0100

    Initial release

---
+++ RiotGame/Assets/Game/Scripts/EnableWeaponInHandScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableWeaponInHandScript : MonoBehaviour
+{
+
+    private GameObject weaponObject;
+    private GameObject rightHand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rightHand = GameObject.FindGameObjectWithTag("RightHand");
+        //weaponObject = gameObject.        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
